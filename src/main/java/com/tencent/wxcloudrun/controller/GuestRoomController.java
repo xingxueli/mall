aab27fa43493712638ecd6ca2cd138ae8eda607d
mike <jackie@hirect.in>
2022-08-28 21:07:03 +0800

    test room/list

--- src/main/java/com/tencent/wxcloudrun/controller/GuestRoomController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/GuestRoomController.java
@@ -10,10 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,7 +27,7 @@ public class GuestRoomController {
         this.logger = LoggerFactory.getLogger(OrderController.class);
     }
 
-    @GetMapping(value = "/list")
+    @PostMapping(value = "/list")
     ApiResponse list(@RequestBody RoomRequest roomRequest) {
         logger.info("GuestRoomController list");
         final RoomResponse roomList = guestRoomService.getRoomList(roomRequest);
