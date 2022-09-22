d9841f57cb254f995ac675196aeba0f760539d46
mike <jackie@hirect.in>
2022-09-22 10:29:52 +0800

    add more

--- src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
@@ -10,14 +10,12 @@ import com.tencent.wxcloudrun.service.CounterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("message")
 public class MessageController {
 
   final Logger logger;
